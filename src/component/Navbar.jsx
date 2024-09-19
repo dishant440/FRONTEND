36d98cf0e09c246606a1845938f9dd6593e5eacc
Dishant <dishantd442442@gmail.com>
2024-09-19 12:20:28 +0530

    created pages folder and installed react router dom

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () =>{
     return (
         <>
             <div className="navbar bg-black h-16 w-full">
-                <div className="flex flex-row justify-between ">                    
+                <div className="flex flex-row justify-between">                    
                     <div><Logo/></div>
                     <div className="my-4 mx-10"><User/></div>
                 </div>
