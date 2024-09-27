f8e7e42f70e338344860e0944f260b738b90c589
Dishant <dishantd442442@gmail.com>
2024-09-27 15:48:28 +0530

    created hook to fetch engineers

--- src/hooks/useServiceEngineer.js
+++ src/hooks/useServiceEngineer.js
@@ -6,6 +6,29 @@ export const useServiceEngineer = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
+
+  const getServiceEngineer = async () =>{
+    setLoading(true);
+    setError("");
+    const token = localStorage.getItem("token");
+    try {
+        const response = await axios.get("http://192.168.29.65:7000/api/getServiceEngineer",{
+            headers:{
+                Authorization:`Bearer ${token}`
+            }
+        });
+        console.log(response.data);        
+        return response.data;
+
+    } catch (error) {
+        setError(err.response?.data?.message || "Failed to add service engineer");
+        toast.error(error || "Failed to add service engineer");
+    }finally {
+        setLoading(false);
+      }
+    return {loading, error, getServiceEngineer}
+  }
+
   const addServiceEngineer = async (engineerData) => {
     setLoading(true);
     setError("");
