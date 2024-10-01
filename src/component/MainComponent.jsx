c26890a830d837397ec5975fe1261289ad210841
Dishant <dishantd442442@gmail.com>
2024-10-01 15:39:14 +0530

    removed commented code

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -1,38 +1,3 @@
-// import React, { useState } from "react";
-// import Heading from "./Heading";
-// import FolderList from "./FolderList";
-
-// const MainComponent = ({ setParentFolderId }) => {
-//   const [refreshKey, setRefreshKey] = useState(0);
-//   console.log("MAIN COMPONENT RE RENDER");
-  
-//   // Function to refresh the FolderList
-//   const refreshFolders = () => {
-//     setRefreshKey((prevKey) => prevKey + 1);
-//   };
-
-//   return (
-//     <div className="flex flex-col">
-//       <Heading  />
-//       <div className="flex justify-center">
-        
-//         <button
-//           className="bg-black font-bold text-amber-400  py-2 rounded-md mr-36 w-24 items-center "
-//           onClick={refreshFolders}
-//           >
-//             REFRESH
-//         </button>
-//         </div>
-//       <FolderList setParentFolderId={setParentFolderId} refreshKey={refreshKey} />
-//     </div>
-//   );
-// };
-
-// export default React.memo(MainComponent);
-
-
-
-
 import React, { useState } from "react";
 import Heading from "./Heading";
 import FolderList from "./FolderList";
